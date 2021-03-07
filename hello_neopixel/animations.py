d57658173966c0d236c6131da6c4d9a20ab42c6d
OpenBagTwo <openbagtwo@gmail.com>
2021-03-07 09:34:10 -0500

    Implement shutdown behavior

--- hello_neopixel/animations.py
+++ hello_neopixel/animations.py
@@ -12,6 +12,7 @@ def random_cycle(
     runtime: float = None,
     transition_time: float = 1.0,
     frame_rate: float = 60.0,
+    clear_after: bool = True,
 ) -> None:
     """Generates a random color for each pixel and cycles them across the strip
 
@@ -26,6 +27,8 @@ def random_cycle(
                                            next. Defaults to 1.0 seconds.
         frame_rate (float, optional): how many times per second to render the
                                       animation. Default is 60 fps.
+        clear_after (bool, optional): whether to turn off all the LED lights
+                                      once the animation ends. Default is True.
 
     Returns:
         None
@@ -42,22 +45,28 @@ def random_cycle(
 
     start_time = utime.ticks_ms()
 
-    while True:
-        current_time = utime.ticks_diff(utime.ticks_ms(), start_time)
-        pixel_shift = int(current_time / transition_time / 1000) % n_pixels
+    try:
+        while True:
+            current_time = utime.ticks_diff(utime.ticks_ms(), start_time)
+            pixel_shift = int(current_time / transition_time / 1000) % n_pixels
 
-        shift_progress = (current_time / transition_time / 1000) % 1
+            shift_progress = (current_time / transition_time / 1000) % 1
 
-        for i in range(n_pixels):
-            light_strip[i] = utils.crossfade(
-                colors[(i + pixel_shift) % n_pixels],
-                colors[(i + pixel_shift + 1) % n_pixels],
-                progress=shift_progress,
-            )
-        light_strip.write()
+            for i in range(n_pixels):
+                light_strip[i] = utils.crossfade(
+                    colors[(i + pixel_shift) % n_pixels],
+                    colors[(i + pixel_shift + 1) % n_pixels],
+                    progress=shift_progress,
+                )
+            light_strip.write()
 
-        # TODO: see if uPython has math.nan
-        if runtime is not None and current_time > runtime:
-            break
+            # TODO: see if uPython has math.nan
+            if runtime is not None and current_time > runtime:
+                break
 
-        utime.sleep_ms(step_time)
+            utime.sleep_ms(step_time)
+    finally:
+        if clear_after:
+            for i in range(n_pixels):
+                light_strip[i] = (0, 0, 0)
+            light_strip.write()
