f66cc90f451e866244ee3d5100cc501ab2e2a0ce
OpenBagTwo <openbagtwo@gmail.com>
2021-03-15 00:45:01 -0400

    Apply the abstraction to the existing animation

--- hello_neopixel/animations.py
+++ hello_neopixel/animations.py
@@ -116,7 +116,6 @@ def run_animations(
     runtime: float = None,
     frame_rate: float = 60.0,
     clear_after: bool = True,
-    write_kwargs: dict = None,
 ) -> None:
     """Run one or more animations for a length of time.
 
@@ -130,9 +129,6 @@ def run_animations(
                                       animations. Default is 60 fps.
         clear_after (bool, optional): whether to turn off all the LED lights
                                       once the animation ends. Default is True.
-        write_kwargs (dict, optional): any additional parameters to pass into
-                                       the write() functions of the LED
-                                       strips. Primarily to be used for testing.
 
     Returns:
         None
@@ -143,9 +139,6 @@ def run_animations(
     )  # type: list
 
     light_strips = {pixel.light_strip for pixel in pixels}
-
-    for animation in animations:
-        light_strips = light_strips | animation.light_strips
 
     step_time_us = int(1e6 / frame_rate)  # microseconds between steps
 
@@ -159,7 +152,7 @@ def run_animations(
                 animation.render(current_time)
 
             for light_strip in light_strips:
-                light_strip.write(**write_kwargs)
+                light_strip.write()
             # TODO: see if uPython has math.nan
             if (
                 runtime is not None
@@ -175,7 +168,53 @@ def run_animations(
             for pixel in pixels:
                 pixel.blank()
             for light_strip in light_strips:
-                light_strip.write(**write_kwargs)
+                light_strip.write()
+
+
+class RandomCycle(Animation):
+    """An animation which starts by generating a random color for each pixel
+    on the strip and then moves each color around the strip in a loop.
+
+    Attributes:
+        colors (list of three-int tuples): the colors used for the animation
+    """
+
+    def __init__(self, pixels, transition_time: float = 1.0):
+        """Initialize the animation
+
+        Args:
+            pixels (list-like of Pixels):
+                the pixels that will be used to render the animation. They do
+                not necessarily need to be on the same strip.
+            transition_time (float, optional):
+                how many seconds it should take for a color to move from one
+                pixel to the next. Defaults to 1.0 seconds.
+        """
+        super().__init__(pixels)
+
+        self.transition_time = transition_time
+
+        self.colors = []
+
+        for i in range(len(pixels)):
+            hue = int(random() * 360)
+            rgb = utils.convert_hue_to_rgb(hue)
+            self.colors.append(rgb)
+
+    def render(self, current_time: float) -> None:
+        shifts = current_time / self.transition_time
+        pixel_shift = int(shifts) % len(self.pixels)
+
+        shift_progress = shifts % 1
+
+        for i, pixel in enumerate(self.pixels):
+            pixel.set(
+                utils.crossfade(
+                    self.colors[(i + pixel_shift) % len(self.pixels)],
+                    self.colors[(i + pixel_shift + 1) % len(self.pixels)],
+                    progress=shift_progress,
+                )
+            )
 
 
 def random_cycle(
@@ -204,47 +243,6 @@ def random_cycle(
     Returns:
         None
     """
-
-    n_pixels = light_strip.n
-    step_time_us = int(1e6 / frame_rate)  # us between steps
-
-    colors = []
-
-    for i in range(n_pixels):
-        hue = int(random() * 360)
-        rgb = utils.convert_hue_to_rgb(hue)
-        colors.append(rgb)
-
-    start_time = utime.ticks_ms()
-
-    try:
-        while True:
-            step_start_tick_us = utime.ticks_us()
-            current_time = utime.ticks_diff(utime.ticks_ms(), start_time) / 1000
-            pixel_shift = int(current_time / transition_time) % n_pixels
-
-            shift_progress = (current_time / transition_time) % 1
-
-            for i in range(n_pixels):
-                light_strip[i] = utils.crossfade(
-                    colors[(i + pixel_shift) % n_pixels],
-                    colors[(i + pixel_shift + 1) % n_pixels],
-                    progress=shift_progress,
-                )
-            light_strip.write()
-
-            # TODO: see if uPython has math.nan
-            if (
-                runtime is not None
-                and current_time + step_time_us / 1e6 > runtime
-            ):
-                break
-
-            compute_time_us = utime.ticks_us() - step_start_tick_us
-
-            utime.sleep_us(step_time_us - compute_time_us)
-    finally:
-        if clear_after:
-            for i in range(n_pixels):
-                light_strip[i] = (0, 0, 0)
-            light_strip.write()
+    pixels = [Pixel(light_strip, i) for i in range(light_strip.n)]
+    animation = RandomCycle(pixels, transition_time)
+    run_animations([animation], runtime, frame_rate, clear_after)
