5c01dc9fae63f4b8e53ef2fb7774e05aa008414f
OpenBagTwo <openbagtwo@gmail.com>
2021-07-31 16:43:13 -0400

    Sample secrets.py

---
+++ projects/ghast/secrets.py
@@ -0,0 +1,5 @@
+"""Secrets needed by your project. Bear in mind: you're storing these as
+plain-text, so anyone with physical access can view its contents."""
+WIFI_SSID = "MySuperSecureWifi"
+WIFI_PASS = "Sup3rS3cr3tP4ssw0rd"
+BLYNK_TOKEN = "dcmetroneverhadsubwaytokenssoimkindajealousofplaceslikenyc"
