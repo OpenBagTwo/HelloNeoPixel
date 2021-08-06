1dee869c43bf1f58a70c555cb7c3976cc0606c1c
OpenBagTwo <openbagtwo@gmail.com>
2021-08-05 22:34:38 -0400

    deploy projects with deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,11 @@
 # and that the board is properly configured.
 
 # Usage:
-# $ sh delploy.sh [-p <port>]
+# $ sh deploy.sh [-p <port>] [project]
 #
 # Arguments:
-#   - p <port> : the port to connect over. Default is /dev/ttyUSB0
+#   -p <port> : the port to connect over. Default is /dev/ttyUSB0
+#   project : optionally specify a project folder to upload
 #
 
 port=/dev/ttyUSB0
@@ -16,7 +17,7 @@ while getopts ":hp:" arg; do
   case ${arg} in
     h )
       echo "Usage:"
-      echo "    sh delploy.sh [-p <port>]"
+      echo "    sh delploy.sh [-p <port>] project"
       exit 0
       ;;
     p )
@@ -32,9 +33,20 @@ while getopts ":hp:" arg; do
       ;;
   esac
 done
+shift $(($OPTIND - 1))
+project=$*
 python setup.py build
 cd build/lib || exit
 echo "Contents of file system:"
 ampy --port "$port" ls || exit
 ampy --port "$port" rmdir hello_neopixel
 ampy --port "$port" put hello_neopixel
+if [ -z "$project" ]; then
+  exit 0
+fi
+cd ../../projects/"$project"
+for f in *.py; do
+  if [ $f != "__init__.py" ]; then
+    ampy --port "$port" put $f
+  fi
+done
